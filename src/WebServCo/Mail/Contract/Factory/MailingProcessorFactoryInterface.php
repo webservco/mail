5c9c3ca8345d32240d384350ebbd3044e0bf5cf0
Radu Maier <ariademur@webserv.co>
2024-09-17 14:39:10 +0200

    MailingProcessorFactoryInterface

---
+++ src/WebServCo/Mail/Contract/Factory/MailingProcessorFactoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\Mail\Contract\Factory;
+
+use WebServCo\Mail\Contract\Processor\MailingProcessorInterface;
+
+interface MailingProcessorFactoryInterface
+{
+    public function creatMailingProcessor(): MailingProcessorInterface;
+}
