77e9cf9dddc1f8c8467ea07b1abefc3a36027325
Radu Maier <ariademur@webserv.co>
2024-10-15 11:17:16 +0200

    MailingTableNameServiceInterface: use in MailingProcessorFactoryInterface

--- src/WebServCo/Mail/Contract/Factory/MailingProcessorFactoryInterface.php
+++ src/WebServCo/Mail/Contract/Factory/MailingProcessorFactoryInterface.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace WebServCo\Mail\Contract\Factory;
 
 use WebServCo\Mail\Contract\Processor\MailingProcessorInterface;
+use WebServCo\Mail\Contract\Service\MailingTableNameServiceInterface;
 
 interface MailingProcessorFactoryInterface
 {
-    public function createMailingProcessor(): MailingProcessorInterface;
+    public function createMailingProcessor(
+        MailingTableNameServiceInterface $tableNameService,
+    ): MailingProcessorInterface;
 }
